4fb14cfb540eb24a40d6eb8ef492c5f849a7f9ea
Louis Thibault <l.thibault@sentimens.com>
2018-03-08 20:52:40 +0000

    Add tests

---
+++ ctx_test.go
@@ -0,0 +1,73 @@
+package ctx
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/magiconair/properties/assert"
+)
+
+func TestBindFunc(t *testing.T) {
+	f := BindFunc(func(d Doner) { panic("called") })
+	assert.Panic(t, func() { f.Bind(nil) }, "called")
+}
+
+func TestDoneChan(t *testing.T) {
+	ch := make(chan struct{})
+	close(ch)
+
+	select {
+	case <-DoneChan(ch).Done():
+	default:
+		t.Error("doner did not reflect closed state of channel")
+	}
+}
+
+func TestDefer(t *testing.T) {
+	ch := make(chan struct{})
+	close(ch)
+
+	chT := make(chan struct{})
+	Defer(Lift(ch), func() { close(chT) })
+
+	select {
+	case <-chT:
+	case <-time.After(time.Millisecond * 100):
+		t.Error("deferred function was not called")
+	}
+}
+
+func TestLink(t *testing.T) {
+	ch := make(chan struct{})
+	close(ch)
+
+	select {
+	case <-Link(Lift(ch), Lift(nil)):
+	case <-time.After(time.Millisecond * 100):
+		t.Error("link did not fire despite a Doner having fired")
+	}
+}
+
+func TestJoin(t *testing.T) {
+	ch := make(chan struct{})
+	close(ch)
+
+	c, cancel := context.WithCancel(context.Background())
+
+	d := Join(Lift(ch), c)
+
+	select {
+	case <-d.Done():
+		t.Error("premature firing of join-Doner")
+	default:
+	}
+
+	cancel()
+
+	select {
+	case <-d.Done():
+	case <-time.After(time.Millisecond * 100):
+		t.Error("join-Doner did not fire despite all constituent Doners having fired")
+	}
+}
