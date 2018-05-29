a2791bad8b83cfd0e31eed23ce68653f74ca7952
Louis Thibault <l.thibault@sentimens.com>
2018-05-29 16:09:28 +0100

    Return ctx.C from New(). Add WithContext() Lazy load signal channel (https://github.com/SentimensRG/ctx/pull/4)

--- sigctx/sigctx.go
+++ sigctx/sigctx.go
@@ -4,29 +4,41 @@ import (
 	"context"
 	"os"
 	"os/signal"
+	"sync"
 	"syscall"
+
+	"github.com/SentimensRG/ctx"
 )
 
-var ctx context.Context
+var (
+	c    ctx.C
+	once sync.Once
+)
 
-func init() {
-	var cancel context.CancelFunc
-	ctx, cancel = context.WithCancel(context.Background())
+// New signal-bound ctx.C that terminates when either SIGINT or SIGTERM
+// is caught.
+func New() ctx.C {
+	once.Do(func() {
+		dc := make(chan struct{})
+		c = dc
 
-	ch := make(chan os.Signal, 1)
-	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+		ch := make(chan os.Signal, 1)
+		signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
 
-	go func() {
-		select {
-		case <-ch:
-			cancel()
-		case <-ctx.Done():
-		}
-	}()
+		go func() {
+			select {
+			case <-ch:
+				close(dc)
+			case <-c.Done():
+			}
+		}()
+	})
+
+	return c
 }
 
-// New signal-bound context.  Context terminates when either SIGINT or SIGTERM
-// is caught.
-func New() context.Context {
-	return ctx
+// NewContext calls New and wraps the result in a context.Context.  The result
+// is a context that fires when either SIGINT or SIGTERM is caught.
+func NewContext() context.Context {
+	return ctx.AsContext(New())
 }
