2f073c7124c9a0dc724bf2e06553911b9156497f
Louis Thibault <l.thibault@sentimens.com>
2018-05-29 16:52:24 +0100

    Fix channel tests

--- refctx/refctx_test.go
+++ refctx/refctx_test.go
@@ -1,35 +1,45 @@
 package refctx
 
 import (
-	"context"
 	"testing"
+	"time"
+
+	"github.com/SentimensRG/ctx"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestRefCtx(t *testing.T) {
 	t.Run("IsInitializedToZero", func(t *testing.T) {
-		c, rc := WithRefCount(context.Background())
+		c, rc := WithRefCount(ctx.C(make(chan struct{})))
 
 		rc.Incr()
+		assert.Equal(t, rc.refcnt, uint32(1))
+
 		rc.Decr()
+		assert.Zero(t, rc.refcnt)
 
 		select {
 		case <-c.Done():
-		default:
-			t.Error("Incr followed by Decr did not release Context")
+		case <-time.After(time.Microsecond):
+			t.Error("Incr followed by Decr did not release Doner")
 		}
 	})
 
 	t.Run("Add", func(t *testing.T) {
-		var batchSize int32 = 10
+		var batchSize uint32 = 10
 
-		c, rc := WithRefCount(context.Background())
+		c, rc := WithRefCount(ctx.C(make(chan struct{})))
+
 		rc.Add(batchSize)
+		assert.Equal(t, rc.refcnt, batchSize)
+
 		rc.Add(-batchSize)
+		assert.Zero(t, rc.refcnt)
 
 		select {
 		case <-c.Done():
-		default:
-			t.Error("Batch incr/decr did not release Context")
+		case <-time.After(time.Microsecond):
+			t.Error("Batch incr/decr did not release Doner")
 		}
 	})
 }
