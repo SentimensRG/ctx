08961ee8dfdf6bd12a91e616016c529b7377ecf4
Louis Thibault <l.thibault@sentimens.com>
2018-05-29 17:52:05 +0100

    Eager-load

--- sigctx/sigctx.go
+++ sigctx/sigctx.go
@@ -4,36 +4,32 @@ import (
 	"context"
 	"os"
 	"os/signal"
-	"sync"
 	"syscall"
 
 	"github.com/SentimensRG/ctx"
 )
 
-var (
-	c    ctx.C
-	once sync.Once
-)
+var c ctx.C
+
+func init() {
+	dc := make(chan struct{})
+	c = dc
+
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		select {
+		case <-ch:
+			close(dc)
+		case <-c.Done():
+		}
+	}()
+}
 
 // New signal-bound ctx.C that terminates when either SIGINT or SIGTERM
 // is caught.
 func New() ctx.C {
-	once.Do(func() {
-		dc := make(chan struct{})
-		c = dc
-
-		ch := make(chan os.Signal, 1)
-		signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
-
-		go func() {
-			select {
-			case <-ch:
-				close(dc)
-			case <-c.Done():
-			}
-		}()
-	})
-
 	return c
 }
 
